dd09d01d2e6dec92d22d517422ff5b496a187cd0
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-19 12:03:13 -0400

    21.10-dev-21

--- api.php
+++ api.php
@@ -153,14 +153,15 @@ class contactsAPI extends CRUDAPI {
 						$count = 0;
 						foreach($relationships as $id => $relations){
 							foreach($relations as $relation){
+								var_dump([
+									'relation' => $relation,
+									'id' => $id,
+									'count' => $count,
+									'organization' => $data['organization'],
+								]);
 								if($relation['relationship'] == 'organizations'){
 									$count++;
 									if($relation['link_to'] == $data['organization']){
-										var_dump([
-											'relation' => $relation,
-											'id' => $id,
-											'count' => $count,
-										]);
 										$result = $this->Auth->delete('relationships', $id);
 										$results = [
 											"success" => $this->Language->Field["Record successfully removed from organization"],
