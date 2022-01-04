0cab649343e0e9e6ca09905b4df19bf02ede3e38
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 16:18:20 -0500

    22.01-dev-140

--- helper.php
+++ helper.php
@@ -10,14 +10,14 @@ class contactsHelper extends Helper {
 						$recordDetail = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
 						if($recordDetail->numRows() > 0){
 							$recordDetail = $recordDetail->fetchAll()->All()[0];
-							$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
+							$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$details['link_to']] = $recordDetail;
 						}
 					}
 					if($details['relationship'] == 'event_attendances' && isset($this->Settings['plugins']['events']['status']) && $this->Settings['plugins']['events']['status']){
 						$recordDetail = $this->Auth->query('SELECT * FROM `event_attendances` WHERE `id` = ?',$details['link_to']);
 						if($recordDetail->numRows() > 0){
 							$recordDetail = $recordDetail->fetchAll()->All()[0];
-							$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
+							$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$details['link_to']] = $recordDetail;
 						}
 					}
 				}
