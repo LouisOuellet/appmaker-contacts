a2fa3075a40e9e2e94b8c022177179e9df7fd1d0
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 14:43:31 -0500

    22.01-dev-134

--- api.php
+++ api.php
@@ -1,28 +1,28 @@
 <?php
 class contactsAPI extends CRUDAPI {
 
-	// public function buildRelation($relations,$relation){
-	// 	$relationships = $this->getRelationships($relation['relationship'],$relation['link_to']);
-	// 	foreach($relationships as $id => $links){
-	// 		foreach($links as $details){
-	// 			if(isset($this->Settings['plugins'][$details['relationship']]['status']) && $this->Settings['plugins'][$details['relationship']]['status']){
-	// 				$recordDetail = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
-	// 				if($recordDetail->numRows() > 0){
-	// 					$recordDetail = $recordDetail->fetchAll()->All()[0];
-	// 					$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
-	// 				}
-	// 			}
-	// 			if($details['relationship'] == 'event_attendances' && isset($this->Settings['plugins']['events']['status']) && $this->Settings['plugins']['events']['status']){
-	// 				$recordDetail = $this->Auth->query('SELECT * FROM `event_attendances` WHERE `id` = ?',$details['link_to']);
-	// 				if($recordDetail->numRows() > 0){
-	// 					$recordDetail = $recordDetail->fetchAll()->All()[0];
-	// 					$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-  //   return $relations;
-  // }
+	public function buildRelation($relations,$relation){
+		$relationships = $this->getRelationships($relation['relationship'],$relation['link_to']);
+		foreach($relationships as $id => $links){
+			foreach($links as $details){
+				if(isset($this->Settings['plugins'][$details['relationship']]['status']) && $this->Settings['plugins'][$details['relationship']]['status']){
+					$recordDetail = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
+					if($recordDetail->numRows() > 0){
+						$recordDetail = $recordDetail->fetchAll()->All()[0];
+						$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
+					}
+				}
+				if($details['relationship'] == 'event_attendances' && isset($this->Settings['plugins']['events']['status']) && $this->Settings['plugins']['events']['status']){
+					$recordDetail = $this->Auth->query('SELECT * FROM `event_attendances` WHERE `id` = ?',$details['link_to']);
+					if($recordDetail->numRows() > 0){
+						$recordDetail = $recordDetail->fetchAll()->All()[0];
+						$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
+					}
+				}
+			}
+		}
+    return $relations;
+  }
 
 	public function create($request = null, $data = null){
 		if(isset($data)){
