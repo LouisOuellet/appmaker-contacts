f87ec71bca3ef4af29042d6f0464c52e1c9bc26f
Louis Ouellet <louis@TheMacSwitch.local>
2022-02-03 15:24:58 -0500

    22.02-dev-149

--- helper.php
+++ helper.php
@@ -1,28 +1,23 @@
 <?php
 class contactsHelper extends Helper {
 
-	// public function buildRelation($relations,$relation){
-	// 	$relationships = $this->getRelationships($relation['relationship'],$relation['link_to']);
-	// 	foreach($relationships as $id => $links){
-	// 		foreach($links as $details){
-	// 			if(in_array($details['relationship'],['users'])){
-	// 				if(isset($this->Settings['plugins'][$details['relationship']]['status']) && $this->Settings['plugins'][$details['relationship']]['status']){
-	// 					$recordDetail = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
-	// 					if($recordDetail->numRows() > 0){
-	// 						$recordDetail = $recordDetail->fetchAll()->All()[0];
-	// 						$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$details['link_to']] = $recordDetail;
-	// 					}
-	// 				}
-	// 				if($details['relationship'] == 'event_attendances' && isset($this->Settings['plugins']['events']['status']) && $this->Settings['plugins']['events']['status']){
-	// 					$recordDetail = $this->Auth->query('SELECT * FROM `event_attendances` WHERE `id` = ?',$details['link_to']);
-	// 					if($recordDetail->numRows() > 0){
-	// 						$recordDetail = $recordDetail->fetchAll()->All()[0];
-	// 						$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$details['link_to']] = $recordDetail;
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-  //   return $relations;
-  // }
+	public function buildRelation($relations){
+    if(isset($relations['contacts'])){
+			foreach($relations['contacts'] as $id => $contact){
+				$relationships = $this->getRelationships('contacts',$id);
+				foreach($relationships as $id => $links){
+					foreach($links as $details){
+						if($details['relationship'] == 'users' && isset($this->Settings['plugins'][$details['relationship']]['status']) && $this->Settings['plugins'][$details['relationship']]['status']){
+							$record = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
+							if($record->numRows() > 0){
+								$record = $record->fetchAll()->All()[0];
+								$relations['contacts'][$id][$details['relationship']][$record['id']] = $record;
+							}
+						}
+					}
+				}
+			}
+    }
+    return $relations;
+  }
 }
